920865de6c48a7b33a761ab281f42c47eaeea870
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-05-19 14:31:23 +0530

    #include <stdio.h> int main() {     int l, r,i;     scanf("%d", &l);     scanf("%d", &r);     for (i = l; i <= r; i++) {         int t = i;         int isD = 1;         while (t != 0) {             int d = t % 10;             if ((d == 0) || (i % d != 0)) {                 isD = 0;                 break;             }             t /= 10;         }         if (isD) {             printf("%d ", i);         }     }     return 0; }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 int main() {
-    int l, r;
+    int l, r,i;
     scanf("%d", &l);
     scanf("%d", &r);
-    for (int i = l; i <= r; i++) {
-        int temp = i;
-        int isDivisible = 1;
-        
-        while (temp != 0) {
-            int digit = temp % 10;
-            
-            if (digit == 0 || i % digit != 0) {
-                isDivisible = 0;
+    for (i = l; i <= r; i++) {
+        int t = i;
+        int isD = 1;
+        while (t != 0) {
+            int d = t % 10;
+            if ((d == 0) || (i % d != 0)) {
+                isD = 0;
                 break;
             }
-            
-            temp /= 10;
+            t /= 10;
         }
-        
-        if (isDivisible) {
+        if (isD) {
             printf("%d ", i);
         }
     }
-    
     return 0;
 }
